b19396a4444b8f943253f4499e68977cb761ff98
johnnycwatt <johnnycwatt@gmail.com>
2025-04-19 10:30:28 +0900

    Enhance buy page and sell page. Add filter, sort and search to the buy page, improve the layout of sell page

--- README.md
+++ README.md
@@ -15,6 +15,15 @@ A fictional real estate website built with Django to practice web development sk
 - Templates for "Buy" (property listings), "Sell" (recently sold properties with appraisal request), and "Our People" (team members) are implemented, dynamically displaying data from the `Property` and `Member` models.
 - Next steps: Enhance the templates with additional styling, implement contact and appraisal forms, and set up authentication for admin editing.
 
+## Screenshots
+
+Below are some screenshots showcasing the Seagram Properties website.
+
+### Buy Page
+![Buy Page](docs/screenshots/buy-page.png)
+
+### Sell Page
+![Sell Page](docs/screenshots/sell-page.png)
 
 ## Notes
 This is a personal learning project for practicing Django. The company and data (including people and properties) are fictional and do not intend to represent any real company, person or property.
--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView, FormView
 from .models import Property, Member
 from .forms import ContactForm, SellForm
+from django.db.models import Q
 
 
 # Create your views here.
@@ -12,8 +13,55 @@ class PropertyListView(ListView):
     model = Property
     template_name = 'core/property_list.html'
     context_object_name = 'properties'
-    queryset = Property.objects.filter(status='sale')
+    
+    def get_queryset(self):
+        queryset = Property.objects.filter(status='sale').order_by('-updated_at')
 
+        #Filter by Suburb
+        suburb = self.request.GET.get('suburb')
+        if suburb:
+            queryset = queryset.filter(suburb=suburb)
+
+        #Filter by Bedrooms
+        bedrooms = self.request.GET.get('bedrooms')
+        if bedrooms:
+            queryset = queryset.filter(bedrooms__gte=bedrooms)
+
+        #Filter by Bathrooms
+        bathrooms = self.request.GET.get('bathrooms')
+        if bathrooms:
+            queryset = queryset.filter(bathrooms__gte=bathrooms)
+  
+
+        #Search by keyword
+        query = self.request.GET.get('q')
+        if query:
+            queryset = queryset.filter(
+                Q(address__icontains=query) |
+                Q(description__icontains=query)
+            )
+
+        #Sort
+        sort = self.request.GET.get('sort')
+        if sort == 'price_asc':
+            queryset = queryset.order_by('price')
+        elif sort == 'price_desc':
+            queryset = queryset.order_by('-price')
+        elif sort == 'date_desc':
+            queryset = queryset.order_by('-created_at')
+        elif sort == 'date_asc':
+            queryset = queryset.order_by('created_at')
+        else:
+            queryset = queryset.order_by('-created_at')
+
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['suburb_choices'] = Property._meta.get_field('suburb').choices
+        context['bedroom_choices'] = [(str(i), f"{i}+") for i in range(1, 6)]
+        context['bathroom_choices'] = [(str(i), f"{i}+") for i in range(1, 4)]
+        return context
     
     
 class PropertyDetailView(DetailView):
